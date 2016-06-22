fc5a97fa711e6e71296419edc932f3ce4eb75234
Susannah Waite <susannahwaite2@gmail.com>
2016-06-22 08:47:41 -0500

    functioning to do list

---
+++ todos/routes/todolist.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var todolist = [];
+
+/* GET home page. */
+
+router.get('/', function(req, res, next) {
+ res.render('index', {todolist: todolist});
+});
+
+router.get('/', function(req, res, next) {
+  res.render(todolist);
+});
+
+router.post('/', function(req, res, next) {
+  var newtodo = req.body;
+  todolist.push(newtodo);
+  res.redirect('/');
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('newtodo/new');
+});
+
+module.exports = router;
